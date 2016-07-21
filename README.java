a0b2c18f4f21eaed215dad8c856f5682744ce1a2
NaliniSekar <NaliniSekar@users.noreply.github.com>
2016-07-21 07:17:06 -0700

    Update and rename README.md to README.java

---
+++ README.java
@@ -0,0 +1,16 @@
+# even1
+odd
++#include<stdio.h>
++int main()
++{
++int a,b,i;
++printf("enter the starting and ending numbers between to find the even series");
++scanf("%d%d",&a,&b);
++for(i=a;i<=b;i++)
++{
++if(i%2=0)
++printf("\n %d",i);
++}
++printf("the even series");
++return 0;
++}
